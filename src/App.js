30edb1c94362752c72e73b6c75155c685dbb63c7
hyesom2642 <kimhyesom@naver.com>
2022-09-14 10:07:56 +0900

    💄 Setting theme responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // > styles 
 import './reset.css';
 
+import { ThemeProvider } from 'styled-components';
+import theme from './theme/theme';
+
 // > components 
 import Gnb from './components/Gnb/Gnb';
 import Home from './pages/Home/Home';
@@ -13,12 +16,14 @@ import { Routes, Route } from 'react-router-dom';
 const App = () => {
   return(
     <>
-      <Gnb />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/project" element={<Project />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Gnb />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </ThemeProvider>
     </>
   )
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+const size = {
+    tablet: '768px',
+    desktop: '1200px',
+};
+
+const theme = {
+    tablet: `(min-width: ${size.tablet})`,
+    desktop: `(min-width: ${size.desktop})`
+};
+export default theme;
