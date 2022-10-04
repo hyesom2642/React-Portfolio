0ff535bd69578d25b313296d844d2dcbd8c90954
hyesom2642 <kimhyesom@naver.com>
2022-10-04 17:28:00 +0900

    📝 Add example data

---
+++ src/pages/Project/ProjectData.js
@@ -0,0 +1,28 @@
+const ProjectData = [
+  {
+    name: 'projectName1',
+    imgUrl: "https://pbs.twimg.com/media/DzMlyvoUUAAfHUH.jpg",
+    desc: '',
+  },
+  {
+    name: 'projectName2',
+    imgUrl: "https://pbs.twimg.com/media/ECZAL6bUwAAQwux.jpg",
+    desc: '',
+  },
+  {
+    name: 'projectName3',
+    imgUrl: "https://pbs.twimg.com/media/Egy3gRSUYAIYXni.jpg",
+    desc: '',
+  },
+  {
+    name: 'projectName4',
+    imgUrl: "https://pbs.twimg.com/media/DYATu_sUMAAAR_y.jpg",
+    desc: '',
+  },
+  {
+    name: 'projectName5',
+    imgUrl: "https://pbs.twimg.com/media/E0N4gLQVUAIGFPM.png:small",
+    desc: '',
+  },
+];
+export default ProjectData;
