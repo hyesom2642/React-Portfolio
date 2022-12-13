f7b775a78605faca7720f49a3507e3e3f1012df8
hyesom2642 <kimhyesom@naver.com>
2022-12-13 20:58:40 +0900

    Project Component 틀 완성

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,8 +1,5 @@
-// > styled-components 
+// styled-components
 import styled from 'styled-components';
-
-// > react-carousel 
-import Slider from "react-slick";
 
 // > 
 import { useState } from 'react';
@@ -10,75 +7,80 @@ import { useState } from 'react';
 // > components
 import Screen from '../../components/Screen/Screen';
 import Modal from '../../components/Modal/Modal';
+
+// > data
 import Data from './ProjectData';
 
-const Proejct = () => {
-    const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-        centerPadding: "80px",
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 1000
-    };
+const Project = () => {
+  const [ProjectData] = useState(Data);
+  const [open, setOpen] = useState(false);
+  const [src, setSrc] = useState('');
 
-    const [ProjectData] = useState(Data);
-    const [open, setOpen] = useState(false);
-    const [src, setSrc] = useState('');
+  function modalOpen(e){
+    setOpen(true);
+    setSrc(e.target.currentSrc);
+  };
+  function modalClose(e){
+    setOpen(false);
+  };
 
-    function modalOpen(e){
-        setOpen(true);
-        setSrc(e.target.currentSrc);
-    };
-    function modalClose(){
-        setOpen(false);
-    };
-
-    return(
-        <>
-            <Screen>
-                <Container>
-                    <SliderContainer {...settings}>
-                        {
-                            ProjectData.map((item) => 
-                                <div onClick={modalOpen} key={item.name}>
-                                    <img src={item.imgUrl} alt={item.name} />
-                                </div>
-                            )
-                        }
-                    </SliderContainer>
-                </Container>
-            </Screen>
-            {
-                open === true
-                ? <Modal modalClose={modalClose} ProjectData={ProjectData} src={src} />
-                : null
-            }
-        </>
-    )
+  return (
+    <>
+      <Screen>
+        <Container>
+          {
+            ProjectData.map((item) => 
+              <ProjectItem onClick={modalOpen} key={item.id}>
+                <img
+                  src={item.imgUrl} 
+                  alt={item.name}
+                  name={item.name}
+                  id={item.id}
+                />
+              </ProjectItem>
+            )
+          }
+        </Container>
+      </Screen>
+      {
+        open === true
+        ? <Modal modalClose={modalClose} ProjectData={ProjectData} src={src} /> 
+        : null
+      }
+    </>
+  )
 }
-export default Proejct;
+
+export default Project;
 
 const Container = styled.div`
-    width: 100%;
-    height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  background-color: hotpink;
+
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
 `;
 
-const SliderContainer = styled(Slider)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+const ProjectItem = styled.div`
+  display: block;
+  width: 50%;
+  height: 500px;
+  background-color: hotpink;
+  padding: 10px;
+
+  img {
     width: 100%;
-    height: 350px;
-    background-color: hotpink;
-    
-    img {
-        display: block;
-        width: 300px;
-        height: 100%;
-        cursor: pointer;
-        object-fit: contain;
-    }
+    height: 100%;
+    object-fit: contain;
+    cursor: pointer;
+  }
 `;
