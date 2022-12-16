4efdd1ed288e3f15ddcdb284eaef89525a52e6e2
hyesom2642 <kimhyesom@naver.com>
2022-12-16 18:45:02 +0900

    BottomData의 id 추가

--- src/components/Gnb/BottomGnb/BottomData.js
+++ src/components/Gnb/BottomGnb/BottomData.js
@@ -1,3 +1,4 @@
+// > icon images
 import userIcon from '../../../assets/icons/user.png';
 import folderIcon from '../../../assets/icons/folder.png';
 import noteIcon from '../../../assets/icons/notes.png';
@@ -7,32 +8,37 @@ import appleTvIcon from '../../../assets/icons/apple-tv.png';
 
 const BottomData = [
   {
+    id: 0,
     title: 'profile',
     imageUrl: `${userIcon}`,
     path: '/profile'
   },
   {
+    id: 1,
     title: 'project',
     imageUrl: `${folderIcon}`,
     path: '/project',
   },
   {
+    id: 2,
     title: 'note',
     imageUrl: `${noteIcon}`
   },
   {
+    id: 3,
     title: 'calendar',
     imageUrl: `${calendarIcon}`
   },
   {
+    id: 4,
     title: 'instagram',
     imageUrl: `${instagramIcon}`,
   },
   {
+    id: 5,
     title: 'apple-tv',
     imageUrl: `${appleTvIcon}`
   },
-
 ]
 
 export default BottomData;
